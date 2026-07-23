dc7fd3a911605487f33cc5ada7c86ad3dc902a89
testacademy2024 <sanketumrani14041992@gmail.com>
2026-07-23 08:53:27 +0530

    auto: update 5 files [2026-07-23 03:23:26]
    Co-authored-by: CommandCodeBot <noreply@commandcode.ai>

---
+++ Chapter_09_Loops/63_NestedForLoops.js
@@ -0,0 +1,8 @@
+// Nested the For Loop
+// 1 Array - W
+//Need to learn Array and Functions to understand this concept better.
+for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+        console.log(i, j);
+    }
+}
